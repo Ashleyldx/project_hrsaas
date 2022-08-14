ec2e0558e796e3c6d88ce2b65e3a632f55bd7033
Ashley Liu <ldx363@163.com>
2022-08-14 14:35:17 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+     <span class="demonstration">显示总数</span>
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page.sync="currentPage1"
+      :page-size="100"
+      layout="total, prev, pager, next"
+      :total="1000">
+    </el-pagination>
+    <!-- <router-view /> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
